da0a8de33beaaf7483bc8c1ed60655a79fe62764
eogakrl <93749243+eogakrl@users.noreply.github.com>
2021-11-24 18:51:23 -0300

    segundo commit

---
+++ escolha.js
@@ -0,0 +1,11 @@
+function escolhalenha () {
+    var opcoes = prompt ("1 - Continuar em casa, pois está um dia agravel para não fazer nada. \n2 - Levantar e ir caminhar afinal está um dia lindo lá fora.");
+    if (opcoes == 1) {
+        return location = "index.html"
+    } else if (opcoes == 2) {
+        return location = "./lenhador02.html"
+    } else {
+    alert ('Opção inválida')
+    return escolha ();
+    }
+    }
