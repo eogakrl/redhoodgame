e55cdfe8ab86816e89a75637d69a20d0eea723e0
eogakrl <93749243+eogakrl@users.noreply.github.com>
2021-11-24 20:44:13 -0300

    Segundo commit

--- escolha.js
+++ escolha.js
@@ -1,11 +1,99 @@
 function escolhalenha () {
     var opcoes = prompt ("1 - Continuar em casa, pois está um dia agravel para não fazer nada. \n2 - Levantar e ir caminhar afinal está um dia lindo lá fora.");
     if (opcoes == 1) {
-        return location = "index.html"
+        return location = "./lenhaover.html"
     } else if (opcoes == 2) {
-        return location = "./lenhador02.html"
+        return location = "./lenha02.html"
     } else {
     alert ('Opção inválida')
     return escolha ();
     }
     }
+function escolhalenha1 () {
+        var opcoes = prompt ("1 - Continuar em casa, pois está um dia agravel para não fazer nada. \n2 - Levantar e ir caminhar afinal está um dia lindo lá fora.");
+        if (opcoes == 1) {
+            return location = "./lobo03.html"
+        } else if (opcoes == 2) {
+            return location = "./lenhaover.html"
+        } else {
+        alert ('Opção inválida')
+        return escolha ();
+        }
+        }
+function escolhalenha2 () {
+            var opcoes = prompt ("1 - Continuar em casa, pois está um dia agravel para não fazer nada. \n2 - Levantar e ir caminhar afinal está um dia lindo lá fora.");
+            if (opcoes == 1) {
+                return location = "./lenhawin.html"
+            } else if (opcoes == 2) {
+                return location = "./lenhaover.html"
+            } else {
+            alert ('Opção inválida')
+            return escolha ();
+            }
+            }
+function escolhachapeu () {
+                var opcoes = prompt ("1 - Continuar em casa, pois está um dia agravel para não fazer nada. \n2 - Levantar e ir caminhar afinal está um dia lindo lá fora.");
+                if (opcoes == 1) {
+                    return location = "./chapeu02.html"
+                } else if (opcoes == 2) {
+                    return location = "./chapeuover.html"
+                } else {
+                alert ('Opção inválida')
+                return escolha ();
+                }
+                }
+function escolhachapeu1 () {
+                    var opcoes = prompt ("1 - Continuar em casa, pois está um dia agravel para não fazer nada. \n2 - Levantar e ir caminhar afinal está um dia lindo lá fora.");
+                    if (opcoes == 1) {
+                        return location = "./chapeu03.html"
+                    } else if (opcoes == 2) {
+                        return location = "./chapeuover.html"
+                    } else {
+                    alert ('Opção inválida')
+                    return escolha ();
+                    }
+                    }
+function escolhachapeu2 () {
+                        var opcoes = prompt ("1 - Continuar em casa, pois está um dia agravel para não fazer nada. \n2 - Levantar e ir caminhar afinal está um dia lindo lá fora.");
+                        if (opcoes == 1) {
+                            return location = "./chapeuover.html"
+                        } else if (opcoes == 2) {
+                            return location = "./chapeuwin.html"
+                        } else {
+                        alert ('Opção inválida')
+                        return escolha ();
+                        }
+                        }
+ function escolhalobo () {
+                            var opcoes = prompt ("1 - Continuar em casa, pois está um dia agravel para não fazer nada. \n2 - Levantar e ir caminhar afinal está um dia lindo lá fora.");
+                            if (opcoes == 1) {
+                                return location = "./lobo02.html"
+                            } else if (opcoes == 2) {
+                                return location = "./loboover.html"
+                            } else {
+                            alert ('Opção inválida')
+                            return escolha ();
+                            }
+                            }
+function escolhalobo1 () {
+                                var opcoes = prompt ("1 - Continuar em casa, pois está um dia agravel para não fazer nada. \n2 - Levantar e ir caminhar afinal está um dia lindo lá fora.");
+                                if (opcoes == 1) {
+                                    return location = "./lobo03.html"
+                                } else if (opcoes == 2) {
+                                    return location = "./loboover.html"
+                                } else {
+                                alert ('Opção inválida')
+                                return escolha ();
+                                }
+                                }
+function escolhalobo2 () {
+                                    var opcoes = prompt ("1 - Continuar em casa, pois está um dia agravel para não fazer nada. \n2 - Levantar e ir caminhar afinal está um dia lindo lá fora.");
+                                    if (opcoes == 1) {
+                                        return location = "./loboover.html"
+                                    } else if (opcoes == 2) {
+                                        return location = "./lobowin.html"
+                                    } else {
+                                    alert ('Opção inválida')
+                                    return escolha ();
+                                    }
+                                    }
